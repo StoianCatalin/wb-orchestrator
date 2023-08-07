18e50fea95f93381b99adcb9723f2d6a458fc442
StoianCatalin <stoian.ioan.catalin@gmail.com>
2023-08-07 09:44:45 +0300

    change enb var

--- apps/crawler-suit/src/main.ts
+++ apps/crawler-suit/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {SuitService} from "./services/suit.service";
 
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
   const suitService = app.get(SuitService);
