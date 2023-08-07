b832a1ab99c48febd7076495494c42f8d347719a
StoianCatalin <stoian.ioan.catalin@gmail.com>
2023-08-07 09:47:08 +0300

    headers

--- libs/common/src/utils/downloadFileAndReturnHash.ts
+++ libs/common/src/utils/downloadFileAndReturnHash.ts
@@ -6,10 +6,23 @@ import * as http from "http";
 export function downloadFileAndReturnHash(storagePath, url): Promise<string> {
   const writeStream = fs.createWriteStream(storagePath);
   // const writeStream = fs.createWriteStream(join(__dirname, '../../../', storagePath));
+  const headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'cross-site',
+    'Sec-Fetch-User': '?1',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+  };
 
   return new Promise((resolve, reject) => {
     let client = url.startsWith('http://') ? http : https;
-    client.get(url, (response) => {
+    client.get(url, { headers }, (response) => {
       const hash = crypto.createHash('md5');
       response.on('data', (chunk) => {
         hash.update(chunk);
